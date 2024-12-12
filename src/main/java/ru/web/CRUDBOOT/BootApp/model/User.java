51f077419431a61f4cae8274b6dcc557799fe2fc
Александр <sashka.anoshkin@gmail.com>
2024-12-12 12:23:54 +0300

    lombok fix

--- src/main/java/ru/web/CRUDBOOT/BootApp/model/User.java
+++ src/main/java/ru/web/CRUDBOOT/BootApp/model/User.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Data
-//геттеры и сетеры вручную прописал, потому что в дате они не работают(хз из-за чего, не смог разобраться)
 @Table(name="users")
 @Entity
 public class User {
@@ -26,35 +25,4 @@ public class User {
     @Column(name = "email")
     private String email;
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
